fb9d273a8c29d8c8c90f6fd8620bbd21c00d473e
AnilKumar Byatarayappa <anilkumarb@crimsonlogic.com>
2018-07-14 11:06:46 +0530

    Anil Test

--- src/test/java/helloworld/GreetingControllerTests.java
+++ src/test/java/helloworld/GreetingControllerTests.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.Test
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = GreetingController.class, secure = false)
